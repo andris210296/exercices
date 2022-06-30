871683441dc8f0249f64a7a81acba2d058a8cd62
Andris Andermann <andris.andermann@terra.com.br>
2022-06-30 20:50:58 -0300

    Dynamic Programming - Target Sum

--- src/test/java/exercices/dynamic_programming/DynamicProgramming.java
+++ src/test/java/exercices/dynamic_programming/DynamicProgramming.java
@@ -1,8 +1,9 @@
 package exercices.dynamic_programming;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
-import java.util.HashMap;
+import java.util.*;
 
 import org.junit.Test;
 
@@ -73,7 +74,7 @@ public class DynamicProgramming {
 	@Test
 	public void gridTravelerMemoizeTest() {		
 		
-		assertEquals(Long.valueOf(0),gridTravelerMemoize(0l, 1l,new HashMap<>() ));
+		assertEquals(Long.valueOf(0),gridTravelerMemoize(0l, 1l,new HashMap<>()));
 		assertEquals(Long.valueOf(1),gridTravelerMemoize(1l, 1l, new HashMap<>()));
 		assertEquals(Long.valueOf(3),gridTravelerMemoize(2l, 3l, new HashMap<>()));
 		assertEquals(Long.valueOf(6),gridTravelerMemoize(3l, 3l, new HashMap<>()));
@@ -90,6 +91,239 @@ public class DynamicProgramming {
 		memoHash.put(key, gridTravelerMemoize(m - 1, n, memoHash) + gridTravelerMemoize(m, n - 1, memoHash));
 		
 		return memoHash.get(key);
+	}
+	
+	
+	/* Write a function 'canSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments	 * 
+	 * This function should return a boolean indicating whether or not it is possible to generate the targetSum using numbers from the array
+	 * 
+	 * You may use an element of the array as many times as needed
+	 * You may assume that all input numbers are nonnegative
+	 * 
+	 */
+	
+	@Test
+	public void canSumSimpleTest() {
+		assertTrue(canSumSimple(7l,Arrays.asList(1l, 2l, 3l, 4l, 5l)));
+		assertFalse(canSumSimple(7l,Arrays.asList(2l,4l)));
+		assertTrue(canSumSimple(8l,Arrays.asList(2l,3l,5l)));
+		assertFalse(canSumSimple(300l,Arrays.asList(7l,14l)));
+
+	}
+	
+	// Complexity O(n^m) exponencial  n=array length m=target sum
+	private boolean canSumSimple(Long targetSum, List<Long> list) {
+		if(targetSum == 0) return true;
+		if(targetSum < 0) return false;
+		
+		for (Long numb : list) {
+			Long remainder = targetSum - numb;
+			if(canSumSimple(remainder, list) == true) {
+				return true;
+			}			
+		}
+		
+		return false;
+	}
+	
+	@Test
+	public void canSumMemoTest() {
+		assertTrue(canSumMemo(7l,Arrays.asList(1l, 2l, 3l, 4l, 5l), new HashMap<>()));
+		assertFalse(canSumMemo(7l,Arrays.asList(2l,4l), new HashMap<>()));
+		assertTrue(canSumMemo(8l,Arrays.asList(2l,3l,5l), new HashMap<>()));
+		assertFalse(canSumMemo(300l,Arrays.asList(7l,14l), new HashMap<>()));
+
+	}
+	
+	// Complexity O(n * m) linear  n=array length m=target sum
+	private boolean canSumMemo(Long targetSum, List<Long> list, HashMap<Long, Boolean> memo) {
+		if(memo.get(targetSum) != null) return memo.get(targetSum);
+		if(targetSum == 0) return true;
+		if(targetSum < 0) return false;
+		
+		for (Long numb : list) {
+			Long remainder = targetSum - numb;
+			if(canSumMemo(remainder, list, memo) == true) {
+				memo.put(targetSum, true);
+				return true;
+			}			
+		}
+		memo.put(targetSum, false);
+		return false;
+	}
+	
+	/*
+	 * Write a function 'howSum(targetSum,numbers)' that takes in a targetSum and an array of numbers as arguments.
+	 * 
+	 * The function should return an array containing any combination of elements that add up to exactly the targetSum.
+	 * If there is no combination that adds up to the targetSum, then return null.
+	 * 
+	 * If there are multiple combinations possible, you may return any single one 
+	 * 
+	 */
+	 
+	
+	@Test
+	public void howSumSimpleTest() {
+		
+		List<Long> list1 = Arrays.asList(3l,2l,2l);
+		assertThat(howSumSimple(7l,Arrays.asList(2l,3l))).hasSameElementsAs(list1);
+		
+		List<Long> list2 = Arrays.asList(4l,3l);		
+		assertThat(howSumSimple(7l,Arrays.asList(5l,3l,4l,7l))).hasSameElementsAs(list2);		
+		
+		assertNull(howSumSimple(7l,Arrays.asList(2l,4l)));
+		
+		List<Long> list3 = Arrays.asList(2l,2l,2l,2l);
+		assertThat(howSumSimple(8l,Arrays.asList(2l,3l,5l))).hasSameElementsAs(list3);
+		
+		assertNull(howSumSimple(300l,Arrays.asList(7l,14l)));
+	}
+	
+	
+	
+	// Complexity O(n^m*m) exponencial  n=array length m=target sum
+	private List<Long> howSumSimple(Long targetSum, List<Long> numbers) {
+		if(targetSum == 0) return new ArrayList<Long>();
+		if(targetSum < 0) return null;
+		
+		for (Long num : numbers) {
+			Long remainder = targetSum - num;
+			List<Long> remainderResult = howSumSimple(remainder, numbers);
+			
+			if(remainderResult != null) {
+				remainderResult.add(num);
+				return remainderResult;
+			}			
+		}		
+		return null;
+	}
+	
+	@Test
+	public void howSumMemoTest() {
+		
+		List<Long> list1 = Arrays.asList(3l,2l,2l);
+		assertThat(howSumMemo(7l,Arrays.asList(2l,3l), new HashMap<>())).hasSameElementsAs(list1);
+		
+		List<Long> list2 = Arrays.asList(4l,3l);		
+		assertThat(howSumMemo(7l,Arrays.asList(5l,3l,4l,7l), new HashMap<>())).hasSameElementsAs(list2);		
+		
+		assertNull(howSumMemo(7l,Arrays.asList(2l,4l), new HashMap<>()));
+		
+		List<Long> list3 = Arrays.asList(2l,2l,2l,2l);
+		assertThat(howSumMemo(8l,Arrays.asList(2l,3l,5l), new HashMap<>())).hasSameElementsAs(list3);
+		
+		assertNull(howSumMemo(300l,Arrays.asList(7l,14l), new HashMap<>()));
+	}	
+	
+	// Complexity O(n*m^2) linear  n=array length m=target sum
+	
+	// 1:48:00 this program contains some flaw that I didn't identify
+	private List<Long> howSumMemo(Long targetSum, List<Long> numbers, HashMap<Long, List<Long>> memo) {
+		if(memo.get(targetSum) != null) return memo.get(targetSum); 
+		if(targetSum == 0) return new ArrayList<Long>();
+		if(targetSum < 0) return null;		
+		for (Long num : numbers) {
+			Long remainder = targetSum - num;
+			List<Long> remainderResult = howSumMemo(remainder, numbers, memo);			
+			if(remainderResult != null) {				
+				remainderResult.add(num);
+				memo.put(targetSum, remainderResult);				
+				return memo.get(targetSum);
+			}			
+		}
+		memo.put(targetSum, null);
+		return null;
+	}
+	
+	/*
+	 * Write a function 'bestSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments
+	 * 
+	 * The function should return an array containing the shortest combination of numbers that add up  to exactly the targetSum
+	 * 
+	 * If there is a tie for the shortest combination, you may return any one of the shortest
+	 * */
+	
+	@Test
+	public void bestSumSimpleTest() {
+		
+		List<Long> list1 = Arrays.asList(7l);
+		assertThat(bestSumSimple(7l,Arrays.asList(5l,3l,4l,7l))).hasSameElementsAs(list1);
+		
+		List<Long> list2 = Arrays.asList(3l,5l);		
+		assertThat(bestSumSimple(8l,Arrays.asList(2l,3l,5l))).hasSameElementsAs(list2);				
+		
+		List<Long> list3 = Arrays.asList(4l,4l);
+		assertThat(bestSumSimple(8l,Arrays.asList(1l,4l,5l))).hasSameElementsAs(list3);
+		
+		List<Long> list4 = Arrays.asList(25l,25l,25l,25l);
+		//assertThat(bestSumSimple(100l,Arrays.asList(1l,2l,5l,25l))).hasSameElementsAs(list4);
+	}
+	
+	// Complexity O(n^m*m) exponencial  n=array length m=target sum
+	private List<Long> bestSumSimple(Long targetSum, List<Long> numbers) {
+		if(targetSum == 0) return new ArrayList<Long>();
+		if(targetSum < 0) return null;
+		
+		List<Long> shortestCombination = null;
+		
+		for (Long num : numbers) {
+			Long remainder = targetSum - num;
+			List<Long> remainderCombination = bestSumSimple(remainder, numbers);			
+			if(remainderCombination != null) {				
+				List<Long> combination =  remainderCombination;
+				combination.add(num);
+				
+				if(shortestCombination == null ||combination.size() < shortestCombination.size()) {
+					shortestCombination = combination;
+				}				 
+			}			
+		}
+
+		return shortestCombination;		
+	}
+	
+	@Test
+	public void bestSumMemoTest() {
+		
+		List<Long> list1 = Arrays.asList(7l);
+		assertThat(bestSumMemo(7l,Arrays.asList(5l,3l,4l,7l), new HashMap<>())).hasSameElementsAs(list1);
+		
+		List<Long> list2 = Arrays.asList(3l,5l);		
+		assertThat(bestSumMemo(8l,Arrays.asList(2l,3l,5l), new HashMap<>())).hasSameElementsAs(list2);				
+		
+		List<Long> list3 = Arrays.asList(4l,4l);
+		assertThat(bestSumMemo(8l,Arrays.asList(1l,4l,5l), new HashMap<>())).hasSameElementsAs(list3);
+		
+		List<Long> list4 = Arrays.asList(25l,25l,25l,25l);
+		assertThat(bestSumMemo(100l,Arrays.asList(1l,2l,5l,25l), new HashMap<>())).hasSameElementsAs(list4);
+	}
+	
+	// Complexity O(n*m^2) linear  n=array length m=target sum
+
+	// 2:08:00 this program contains some flaw that I didn't identify
+	private List<Long> bestSumMemo(Long targetSum, List<Long> numbers, HashMap<Long, List<Long>> memo) {
+		if(memo.get(targetSum) != null) return memo.get(targetSum);
+		if(targetSum == 0) return new ArrayList<Long>();
+		if(targetSum < 0) return null;
+		
+		List<Long> shortestCombination = null;
+		
+		for (Long num : numbers) {
+			Long remainder = targetSum - num;
+			List<Long> remainderCombination = bestSumMemo(remainder, numbers, memo);			
+			if(remainderCombination != null) {				
+				List<Long> combination =  remainderCombination;
+				combination.add(num);
+				
+				if(shortestCombination == null ||combination.size() < shortestCombination.size()) {
+					shortestCombination = combination;
+				}				 
+			}			
+		}
+		
+		memo.put(targetSum, shortestCombination);
+		return shortestCombination;
 	}
 
 }
